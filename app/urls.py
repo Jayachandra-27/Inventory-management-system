d4b1334c3f8a842d89825ff48b35504f000e58f4
Jayachandra-27 <jayachandrach2025@gmail.com>
2025-11-28 11:23:50 +0530

    final

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home.as_view(),name='home'),
+    path('',views.signinView.as_view(),name='signin'),
+    path('home/',views.home.as_view(),name='home'),
     path('dashboard/',views.dashboard.as_view(),name='dashboard'),
-    path('signup/',views.SignUpView.as_view(),name='signup'),
     path('edit_item/<int:pk>/',views.edit_item.as_view(),name='edititem'),
     path('delete_item/<int:pk>/',views.delete_item.as_view(),name='deleteitem'),
     path('add-item/', views.add_item.as_view(), name='additem'),
+    #Authentication
+    path('signup/',views.SignUpView.as_view(),name='signup'),
+    path('signin/',views.signinView.as_view(),name='signin'),
+    path('signout/',views.signoutView.as_view(),name='signout'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -7,6 +7,11 @@ from .models import InventoryItem,Category
 from project.settings import low_quantity
 from django.contrib import messages
 from .forms import RegistrationForm,InventoryItemForm
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 from django.contrib.auth import authenticate 
 from django.contrib import auth
 
@@ -14,7 +19,7 @@ from django.contrib import auth
 class home(TemplateView):
     template_name='inventory/home.html'
 
-class dashboard(View):
+class dashboard(LoginRequiredMixin,View):
     def get(self,request):
         items=InventoryItem.objects.all().order_by('-id')
         low_inventory=InventoryItem.objects.filter(quantity__lte=low_quantity)
@@ -33,43 +38,23 @@ class dashboard(View):
         return render(request,'inventory/dashboard.html',context)
     
 
-class SignUpView(View):
-    def get(self,request):
-        form=RegistrationForm
-        context={
-            'form':form
-        }
-        return render(request,'inventory/signup.html',context)
-    
-    def post(self,request):
-        form=RegistrationForm(request.POST)
-        if form.is_valid():
-            user=form.save()
-            username=form.cleaned_data['username']
-            password=form.cleaned_data['password1']
-            user=auth.authenticate(username=username,password=password)
-            if user is not None:
-                auth.login(request,user)
-                messages.success(request, "Account created and logged in.")
-                return redirect('dashboard')
 
-        
-        return render(request,'inventory/signup.html',{'form':form})
+
     
-class edit_item(UpdateView):
+class edit_item(LoginRequiredMixin,UpdateView):
     model=InventoryItem
     form_class=InventoryItemForm
     template_name='inventory/editform.html'
     success_url = reverse_lazy('dashboard')
 
-class delete_item(DeleteView):
+class delete_item(LoginRequiredMixin,DeleteView):
     model=InventoryItem
     # form_class=InventoryItemForm
     template_name='inventory/delete.html'
     success_url = reverse_lazy('dashboard')
     context_object_name = 'item'
 
-class add_item(View):
+class add_item(LoginRequiredMixin,View):
     def get(self, request):
         form = InventoryItemForm()
         return render(request, 'inventory/editform.html', {'form': form})
@@ -80,7 +65,48 @@ class add_item(View):
             form.save()
             return redirect('dashboard')
         return render(request,'inventory/editform.html',{'form':form})
-	# model = InventoryItem
-	# form_class = InventoryItemForm
-	# template_name = 'inventory/editform.html'
-	# success_url = reverse_lazy('dashboard')
+	
+class SignUpView(View):
+    def get(self,request):
+        form=RegistrationForm
+        return render(request,'inventory/signup.html',{'form':form})
+
+
+
+    def post(self,request):
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created — please sign in.")
+            return redirect('signin')
+        
+        return render(request,'inventory/signup.html',{'form':form})
+    
+    
+
+class signinView(View):
+    def get(self, request):
+        form = AuthenticationForm()                  
+        return render(request, 'inventory/signin.html', {'form': form})
+
+    def post(self,request):
+        form=AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+                messages.success(request, "Account logged in.")
+                return redirect('home')
+            messages.error(request, "Invalid credentials.")
+
+        
+        return render(request,'inventory/signin.html',{'form':form})
+    
+
+class signoutView(View):
+    def get(self,request):
+
+        auth.logout(request)
+        return redirect('signin')
