54b2037c001455c0bca5553147b8a4d65f516ad3
Jayachandra-27 <jayachandrach2025@gmail.com>
2025-11-28 12:31:06 +0530

    final

--- project/settings.py
+++ project/settings.py
@@ -77,10 +77,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ims_db',        # your database name
+        'USER': 'root',          # your MySQL username
+        'PASSWORD': 'Root@1002',  # your MySQL password
+        'HOST': '127.0.0.1',     # localhost
+        'PORT': '3306',          # default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
